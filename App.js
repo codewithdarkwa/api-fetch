09811c24bdd2ee8c6e65e11d130bad503c872948
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-04 12:52:19 +0000

    successful data fetching

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
+import React,{useState,useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+const [data,setData] = useState([]);
+const [loading,setLoading] = useState(true)
+
+ const url = "https://jsonplaceholder.typicode.com/posts"
+
+ useEffect(()=>{
+   fetch(url)
+   .then(response => response.json())
+   .then((json)=>setData(json))
+   .catch((error)=>console.log(error))
+   .finally(()=> setLoading(false))
+ },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        loading ? <Text>Loading ...</Text>:
+        data.map((post)=>(
+          <View style={{flex:1,alignItems: 'center',justifyContent: 'center'}}>
+            <Text style={{fontSize:30, fontWeight: 'bold'}}>{post.title}</Text>
+            <Text style={{fontSize:15, color:'blue'}} >{post.body}</Text>
+          </View>
+        ))
+      }
     </View>
   );
 }
